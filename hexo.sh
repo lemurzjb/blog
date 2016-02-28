cd3b92b4dd107753c46c4348d3c551b118b89756
lemurzjb <lemurzjb@hotmail.com>
2016-02-28 13:54:20 +0800

    Blog updated

--- hexo.sh
+++ hexo.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-# start hexo server
-hexo server -s &
+#生成静态页面
 
-hexo generate --watch
+hexo generate
+#将html目录加入git版本管理
+git add html
+
+git commit -am "Blog updated"
+
+git push github-ssh master
